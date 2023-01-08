50bb252ebd1d8dce67dd1ac0d2f4016cb9db37c7
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-01-09 00:04:03 +0100

    header image overlay

--- ui/src/components/HeaderImage.vue
+++ ui/src/components/HeaderImage.vue
@@ -1,4 +1,6 @@
 <script setup>
+import DynamicNavigation from './DynamicNavigation.vue';
+
 const props = defineProps({
   image: {
     type: Object,
@@ -14,6 +16,10 @@ const props = defineProps({
       :src="props.image?.url"
       :alt="props.image?.filename"
     />
+    <div class="page-header-image-overlay">
+      <button type="button" class="back-button" aria-label="back" />
+      <DynamicNavigation type="social" />
+    </div>
   </header>
 </template>
 
@@ -25,10 +31,35 @@ header {
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
 }
 .page-header-image {
   width: 100%;
   height: auto;
   min-height: 2rem;
+}
+.page-header-image-overlay {
+  position: absolute;
+  max-width: var(--page-max-width);
+  top: 2rem;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.back-button {
+  appearance: none;
+  border: none;
+  background: none;
+  color: var(--color-white);
+  font-family: sans-serif;
+  font-size: 2rem;
+}
+.back-button::before {
+  content: '\003C';
+}
+nav.social {
+  display: flex;
+  flex-direction: row;
 }
 </style>
