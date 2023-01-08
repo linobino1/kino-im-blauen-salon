b7441a953e0f1abee1a5a7d91f94ca2ad664896e
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-01-08 20:55:39 +0100

    enable drafts for screenings

--- api/src/collections/Screenings.ts
+++ api/src/collections/Screenings.ts
@@ -5,11 +5,26 @@ const Screenings: CollectionConfig = {
   slug: 'screenings',
   admin: {
     group: 'Screenings',
-    defaultColumns: ['date', 'time', 'title'],
+    defaultColumns: ['date', 'time', 'title', '_status'],
     useAsTitle: 'title',
   },
   access: {
-    read: () => true,
+    read: ({ req }) => {
+      // admin -> everything
+      if (req.user && req.user.role === 'admin') return true;
+
+      // not logged in or not admin -> only published
+      return {
+        _status: {
+          equals: 'published',
+        },
+      };
+    },
+  },
+  versions: {
+    drafts: {
+      autosave: true,
+    },
   },
   fields: [
     {
