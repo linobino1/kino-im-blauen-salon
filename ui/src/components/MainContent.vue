cf6920cb88547904806fca81bd55a8da9f309044
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-01-08 02:44:34 +0100

    cleanup

--- ui/src/components/MainContent.vue
+++ ui/src/components/MainContent.vue
@@ -5,11 +5,20 @@
   <div class="main-content"><slot /></div>
 </template>
 
-<style scoped>
+<style>
 .main-content {
-  width: var(--page-max-width);
+  width: min(100vw, var(--page-max-width));
   margin: 0 auto;
   margin-top: 3rem;
   color: var(--color-white);
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+@media (max-width: 700px) {
+  .main-content {
+    width: 100%;
+  }
+}
+@media (min-width: 1024px) {
 }
 </style>
