0b88958dddf5c9af1be41616831aac574a092ead
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-02-02 17:12:44 +0100

    beautified user status

--- components/UserStatus/index.tsx
+++ components/UserStatus/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
-import { signIn, useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import { _t } from '../../i18n';
+import classes from '../../css/userstatus.module.css';
 
 export const UserStatus: React.FC = () => {
   const { data: session } = useSession();
 
-  return session ? (
-    <Link href="/auth/me">{session.user.email}</Link>
-  ) : (
-    <button type="button" onClick={() => signIn()}>{_t('Sign in')}</button>
+  return (
+    <div className={classes.userStatus}>
+      { session ? (
+        <Link href="/auth/me" className={classes.email}>{session.user.email}</Link>
+      ) : (
+        <Link href="/auth/signin" className={classes.signIn}>
+          {_t('Sign In')}
+        </Link>
+      )}
+    </div>
   );
 };
