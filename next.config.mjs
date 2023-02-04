9dc82d9235a98f350ad501222e7c3901c8caa504
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-02-04 13:42:32 +0100

    cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  publicRuntimeConfig: {
-    SERVER_URL: process.env.PAYLOAD_PUBLIC_SERVER_URL || 'http://localhost:3000',
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
